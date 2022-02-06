8067f630f872afe6d0be2eae75d0a35308198ee9
BekirGurkanGuldas <gurkanguldas@gmail.com>
2022-02-06 15:04:35 +0300

    Data Transfer Object Layer Created

--- src/main/java/com/gurkanguldas/innovaspringhomework/controller/CountryController.java
+++ src/main/java/com/gurkanguldas/innovaspringhomework/controller/CountryController.java
@@ -1,5 +1,6 @@
 package com.gurkanguldas.innovaspringhomework.controller;
 
+import com.gurkanguldas.innovaspringhomework.dto.CountryDto;
 import com.gurkanguldas.innovaspringhomework.entity.CountryEntity;
 import com.gurkanguldas.innovaspringhomework.repository.CountryRepository;
 import lombok.extern.log4j.Log4j2;
@@ -23,18 +24,29 @@ public class CountryController
     @GetMapping("/country/register")
     public String getRegister(Model model)
     {
-        model.addAttribute("key_form", new CountryEntity());
+        model.addAttribute("key_form", new CountryDto());
         return "register";
     }
 
     @PostMapping("/country/register")
-    public String postForm(@Valid @ModelAttribute("key_form") CountryEntity entity, BindingResult bindingResult) {
+    public String postForm(@Valid @ModelAttribute("key_form") CountryDto dto, BindingResult bindingResult) {
 
         if(bindingResult.hasErrors()){
             log.error("Hata var");
-            log.info(entity);
+            log.info(dto);
             return "register";
         }
+
+        CountryEntity entity = CountryEntity.builder()
+                .countryId(dto.getCountryId())
+                .countryArea(dto.getCountryArea())
+                .countryPopulation(dto.getCountryPopulation())
+                .countryContinent(dto.getCountryContinent())
+                .countryFoundingYear(dto.getCountryFoundingYear())
+                .countryName(dto.getCountryName())
+                .countryNeighbors(dto.getCountryNeighbors())
+                .build();
+
         countryRepository.save(entity);
         log.info(entity);
         return "register";
